19ddd3940eb87d4cc4edcaedbf2950d6dae0039a
satorchi <steve@satorchi.net>
2017-07-19 16:19:27 +0200

    wrapper build script

--- build.sh
+++ build.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
-
-# define where to find the libraries.  Normally it should be in libs.new, one directory up.
+# $Id: build.sh
+# $auth: Steve Torchinsky <satorchi@apc.in2p3.fr>
+# $created: Wed 19 Jul 2017 15:32:22 CEST
+# $license: GPLv3 or later, see https://www.gnu.org/licenses/gpl-3.0.txt
+# 
+#           This is free software: you are free to change and
+#           redistribute it.  There is NO WARRANTY, to the extent
+#           permitted by law.
+#
+# wrapper script to build the pystudio and qubicpack packages
+# this is a bit of a hack to ensure we correctly point to the QubicStudio libraries
+#
+# Note that this wrapper does a full clean and rebuilds the package!
 
 BUILD_SH=`echo $0|awk -F/ '{print $NF}'`
 PYSTUDIO_RELDIR=`echo $0|sed -ne "s/${BUILD_SH}//" -ep`
-LIB_NEW=lib.new
 
 cd $PYSTUDIO_RELDIR
 PYSTUDIO_DIR=`pwd -P`
-cd ../
-if [ ! -d "${LIB_NEW}" ];then
-    echo "could not find library directory"
+QUBICSTUDIO_LIB=QubicStudio.lib
+
+if [ ! -d "${QUBICSTUDIO_LIB}" ];then
+    echo "could not find library directory!  $QUBICSTUDIO_LIB"
     exit
 fi
 
-cd ${LIB_NEW}
+cd ${QUBICSTUDIO_LIB}
 LIBDISPATCHERACCESS=`pwd -P`
 
 cd $PYSTUDIO_DIR
