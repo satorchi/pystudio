5ccfd29854983aee2cbb245c3ca6a27d55cfce83
satorchi <steve@satorchi.net>
2017-09-06 14:56:19 +0200

    bug fix for viewer command

--- scripts/run_iv.py
+++ scripts/run_iv.py
@@ -80,7 +80,7 @@ for viewer in viewers:
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
     if not out=='':
-        use_viewer=out
+        use_viewer=out.strip()
         break
         
 cmd='%s %s' % (use_viewer,pdfname)
