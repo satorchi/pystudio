84224cb1a8fd2df8667814b370a3a1bb723986da
satorchi <steve@satorchi.net>
2017-10-30 08:51:17 +0100

    wrapper script to run fast I-V

---
+++ scripts/fast_iv.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+'''
+$Id: fast_iv.py
+$auth: Steve Torchinsky <satorchi@apc.in2p3.fr>
+$created: Mon 30 Oct 2017 08:31:17 CET
+$license: GPLv3 or later, see https://www.gnu.org/licenses/gpl-3.0.txt
+
+          This is free software: you are free to change and
+          redistribute it.  There is NO WARRANTY, to the extent
+          permitted by law.
+
+wrapper script to run an I-V measurement using the "fast" method
+'''
+from __future__ import division, print_function
+from qubicpack import qubicpack as qp
+import matplotlib.pyplot as plt
+import datetime as dt
+import subprocess,os,sys,time
+import numpy as np
+reload(sys)
+sys.setdefaultencoding('utf8')
+from satorchipy.datefunctions import *
+
+go=qp()
+go.debuglevel=1
+
+
+# can I get ASIC from QubicStudio?
+asic=go.get_from_keyboard('Which ASIC?  ',2)
+if asic==None:quit()
+ret=go.assign_asic(asic)
+
+go.assign_integration_time(240)
+go.configure_PID()
+go.get_iv_timeline(vmin=0.5,vmax=3.0)
+
+for idx in range(go.NPIXELS):
+    TES=idx+1
+    go.timeline2adu(TES)
+
+plt.ion()
+go.plot_iv(70)
+go.write_fits()
