1a3bc95240adf8afe31b68f5bdbf849f4e6b3970
Steve Torchinsky <satorchi@apc.in2p3.fr>
2018-01-26 12:00:36 +0100

    command to change relay 10kOhm/100kOhm

--- pystudio/dispatcheraccess.pyx
+++ pystudio/dispatcheraccess.pyx
@@ -647,6 +647,18 @@ cdef class DispatcherAccess:
         if not out:
             raise RuntimeError(self.lastError)
 
+    def sendSetRelay(self, int asicNum , int bitmask):
+        """
+        sendSetRelay(int asicNum , int bitmask)
+
+        choose the relay resistance: 100kOhm, 10kOhm
+        """
+        cdef bool out = self._da.sendSetRelay(asicNum, bitmask)
+        if not out:
+            raise RuntimeError(self.lastError)
+        
+
+        
     def sendResetVOffset(self, int asicNum):
         """
         sendResetVOffset(int asicNum)
