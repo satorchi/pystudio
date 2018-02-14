c257e1a6bf881f815a6a826d7b5c82a49abfb29d
satorchi <satorchi@apc.in2p3.fr>
2018-02-14 14:24:59 +0100

    get number of masked samples from rawmask

--- qubicpack/iv.py
+++ qubicpack/iv.py
@@ -55,6 +55,24 @@ def lut(self,v,vmin=3.0,vmax=9.0):
     rgb=colourmap(vfractional)
     return rgb
 
+def n_masked(self):
+    '''
+    find the number of masked samples per integration
+    '''
+    if self.rawmask is None:
+        rawmask_added_date=dt.datetime.strptime('2018-02-14 11:30','%Y-%m-%d %H:%M')
+        if self.obsdate < rawmask_added_date: return 8
+        return 0
+
+    n_masked=0
+    for val in self.rawmask:
+        for idx in range(8):
+            masked = val & 1
+            n_masked+=masked
+            val = val >> 1
+    return n_masked
+        
+
 def ADU2I(self,ADU, offset=None, R1adjust=1.0):
     ''' 
     This is the magic formula to convert the measured output of the TES to current
@@ -63,9 +81,11 @@ def ADU2I(self,ADU, offset=None, R1adjust=1.0):
     Rfb   = 10000. # Ohm
     q_ADC = 20./(2**16-1)
     G_FLL = (10.4 / 0.2) * Rfb
-    I = 1e6 * (ADU / 2**7) * (q_ADC/G_FLL) * (self.nsamples - 8) * R1adjust
+    n_masked=self.n_masked()
+    
+    I = 1e6 * (ADU / 2**7) * (q_ADC/G_FLL) * (self.nsamples - n_masked) * R1adjust
 
-    if offset!=None: return I+offset
+    if not offset is None: return I+offset
     return I
 
 def setup_plot_Vavg(self,axes=None):
