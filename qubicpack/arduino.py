a6796b7f2f0db43e2b71ee027f8889606d342a51
satorchi <satorchi@apc.in2p3.fr>
2018-09-11 12:00:01 +0200

    bugfix

--- qubicpack/arduino.py
+++ qubicpack/arduino.py
@@ -18,7 +18,6 @@ import numpy as np
 from scipy.optimize import curve_fit
 import datetime as dt
 from satorchipy.datefunctions import *
-from qubicpack import qubicpack as qp
 
 def arduino_acquire(self,arduino_t,arduino_a):
     '''
