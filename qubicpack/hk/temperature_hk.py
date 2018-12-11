daffc27645e8b3588e56f8d178b8cc125147a0ab
satorchi <satorchi@apc.in2p3.fr>
2018-12-11 09:21:40 +0100

    get data from temperature diodes

---
+++ qubicpack/hk/temperature_hk.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python
+'''
+$Id: temperature_hk.py
+$auth: Steve Torchinsky <satorchi@apc.in2p3.fr>
+$created: Sun 09 Dec 2018 11:16:58 CET
+
+$former_Id: cryo_data_logV2.py
+$auth: Manuel Gonzalez
+
+$license: GPLv3 or later, see https://www.gnu.org/licenses/gpl-3.0.txt
+
+          This is free software: you are free to change and
+          redistribute it.  There is NO WARRANTY, to the extent
+          permitted by law.
+
+get temperatures from the temperature diodes
+'''
+from __future__ import division, print_function
+import os,sys,serial,subprocess
+from glob import glob
+import time
+import datetime as dt
+import struct
+from shutil import copyfile
+import numpy as np
+from scipy import interpolate
+
+class temperature_hk :
+    ''' a class to gather temperatures from the temperature diodes
+    there are 21 channels, of which 18 are used
+    '''
+    def __init__(self,port='/dev/cryo_temperatures',caldir=None):
+ 
+        self.nchannel = 21
+        self.nT = 18
+        self.ser = None
+        self.connected=False
+        self.port=port
+
+        # these were the headings in the temperature.dat file
+        self.labels=['T01_40K_filters',
+                     'T09_shield_40K_down',
+                     'T05_PT1_stage_1',
+                     'T06_PT2_stage_1',
+                     'T12_shield_40K_right',
+                     'T02_4K_filters',
+                     'T03_HWP1',
+                     'T04_HWP2',
+                     'T10_shield_4K_down',
+                     'T11_shield_4K_right',
+                     'T13_PT1_stage_2',
+                     'T14_PT2_stage_2',
+                     '300mK-4CP-D-1',
+                     '300mK-4HS-D-1',
+                     '300mK-3CP-D-1',
+                     '300mK-3HS-D-1',
+                     '1K-4HS-D-1',
+                     '1K-4CP-D-1']
+
+        self.connected=self.connect(port=port)
+        res = self.setup_calibration(caldir)
+        if not res: self.connected=False
+        return None
+
+    def log(self,msg):
+        '''messages to log file and to screen
+        '''
+        now=dt.datetime.now()
+        logmsg='%s | %s' % (now.strftime('%Y-%m-%d %H:%M:%S'),msg)
+        h=open('hk_temperature.log','a')
+        h.write(logmsg+'\n')
+        h.close()
+        print(logmsg)
+        return
+
+    def connect(self,port=None):
+        ''' connect by serial (USB) to the cryo temperature diode controller
+        interface is defined in /etc/udev/rules.d/99-cryotemperature.rules
+        '''
+        if port is None: port=self.port
+        self.port=port
+        
+        self.connected=False
+        if not os.path.exists(port):
+            self.log('ERROR! Could not find the temperature device: %s' % port)
+            return False
+
+        try:
+            self.ser=serial.Serial(port=port, baudrate=19200, timeout=0.1)
+        except:
+            self.log('ERROR! Could not connect to the temperature device.')
+            return False
+        
+        # wait for activation
+        data_length = 0
+        now=dt.datetime.now()
+        max_wait=dt.timedelta(seconds=30)
+        wait_endtime=now+max_wait
+        while data_length == 0 and now < wait_endtime:
+	    string0 = ser.readline()
+	    data_length = len(string0)
+            now=dt.datetime.now()
+
+            if data_length == 0:
+                self.log("ERROR! Timeout! Could not connect to cryo temperatures!")
+                return False
+
+        self.connected=True        
+        return True
+
+    def disconnect(self):
+        '''close the serial connection to the temperature diodes
+        '''
+        self.ser.close()
+        self.connected=False
+        return
+
+    def setup_calibration(self,caldir=None):
+        '''read the calibration tables
+        '''
+        if caldir is None:
+            datadir = os.getcwd() + '/data'
+            caldir=datadir+'/log_cryo/calibration'
+            
+        if not os.path.isdir(caldir):
+            self.log('ERROR! Could not find calibration directory: %s' % caldir)
+            return False
+
+        calibration_files = map(lambda x: caldir+'/calibration_ch'+str(x).zfill(2)+'.dat',range(self.nT))
+        # check for file existence
+        for fname in calibration_files:
+            if not os.path.exists(fname):
+                self.log('ERROR! Could not find calibration file: %s' % fname)
+                return False    
+        self.calibration_tables = map(lambda x: np.loadtxt(x),calibration_files)
+        
+        self.gain=[0.0626158803,
+                   0.0626095353,
+                   0.0626137451,
+                   0.0626129561,
+                   0.0626046339,
+                   0.0626023651,
+                   0.0626006617,
+                   0.0626026485,
+                   0.0625785803,
+                   0.0625785839,
+                   0.0625793290,
+                   0.0625781058,
+                   0.0625662881,
+                   0.0625695137,
+                   0.0625704064,
+                   0.0625677486,
+                   0.0626280689,
+                   0.0626252146,
+                   0.0626282954,
+                   0.0626263287]
+
+        self.offset=[-2049.6181153598,
+                     -2049.9601678205,
+                     -2049.2013364949,
+                     -2049.6533307630,
+                     -2049.2122546909,
+                     -2049.2859049597,
+                     -2049.5080966974,
+                     -2049.0869777510,
+                     -2048.5491308714,
+                     -2048.5516331324,
+                     -2048.5804165382,
+                     -2048.4528778901,
+                     -2048.5684350553,
+                     -2048.4054581436,
+                     -2048.3698491680,
+                     -2048.3218114873,
+                     -2050.3137988660,
+                     -2050.2215204946,
+                     -2050.2695627877,
+                     -2050.0539409956]
+        
+        return True
+
+    def get_temperatures(self):
+        '''read the temperatures from the diodes
+        '''
+
+        if self.ser is None:
+            self.log('ERROR! Please setup the Temperature Diode device.  run connect()')
+            return None
+        
+        voltageData = []
+        temperatureData = []
+
+        a = self.ser.readline()
+        data_length = len(a)
+        if data_length == 0: return None
+
+        datlist=a.strip().split()        
+        rawData = map(int,datlist)
+        
+        for idx in range(self.nT):
+            voltageData.append(rawData[idx+1]*self.gain[idx]+self.offset[idx])
+            x=self.calibration_tables[idx][:,0]
+            y=self.calibration_tables[idx][:,1] 
+            interpolate_function = interpolate.interp1d(x,y,kind='linear',fill_value='extrapolate')
+            temperatureData.append(interpolate_function(voltageData[idx]))
+
+        return np.array(temperatureData)
+    
+
+
