713562a70334e4860b4fa84a04ee5e066c0a8faf
satorchi <satorchi@apc.in2p3.fr>
2018-12-11 09:50:24 +0100

    use 'Agg' for plots for servers

--- scripts/run_hkclient.py
+++ scripts/run_hkclient.py
@@ -11,6 +11,9 @@ $license: GPLv3 or later, see https://www.gnu.org/licenses/gpl-3.0.txt
 
 run the client for gathering QUBIC housekeeping data sent on the socket
 '''
+import matplotlib
+matplotlib.use('Agg')
+
 from qubicpack.hk.hk_broadcast import hk_broadcast
 bc=hk_broadcast()
 bc.hk_client()
