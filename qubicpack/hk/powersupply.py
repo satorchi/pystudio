b18115407ca4059f1fb2c63e4e2868ea5af72a1c
satorchi <satorchi@apc.in2p3.fr>
2019-01-23 15:42:08 +0100

    bugfix

--- qubicpack/hk/powersupply.py
+++ qubicpack/hk/powersupply.py
@@ -21,7 +21,7 @@ import os,sys,serial,subprocess
 from glob import glob
 import numpy as np
 import datetime as dt
-
+import re
 import readline
 readline.parse_and_bind('tab: complete')
 readline.parse_and_bind('set editing-mode vi')
