7e3e897ba10501f3c1fe73f33b1182dc3629156d
satorchi <satorchi@apc.in2p3.fr>
2019-03-18 17:52:03 +0100

    qubicpack is now in separate repository

--- setup_qubicpack.py
+++ setup_qubicpack.py
@@ -30,51 +30,8 @@ VERSION          = '2.0.0'
 with open('README.md') as f:
     long_description = f.readlines()
 
-
-setup(install_requires=['numpy'],
-      name=DISTNAME,
-      version=VERSION,
-      packages=[DISTNAME, DISTNAME+'/hk'],
-      package_data={DISTNAME: ['data/*']},
-      zip_safe=False,
-      author=AUTHOR,
-      author_email=AUTHOR_EMAIL,
-      maintainer=MAINTAINER,
-      maintainer_email=MAINTAINER_EMAIL,
-      description=DESCRIPTION,
-      license=LICENSE,
-      url=URL,
-      download_url=DOWNLOAD_URL,
-      long_description=long_description,
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Environment :: Console',
-          'Intended Audience :: Developers',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: GNU General Public License (GPL)',
-          'Topic :: Scientific/Engineering'],
-)
-
-# install the executable scripts
-exec_dir = '/usr/local/bin'
-scripts = ['scripts/calsource_commander.py',
-           'calsource_set_frequency.py',
-           'calsource_step_frequency.py',
-           'qubicpack/hk/powersupply.py',
-           'qubicpack/copy_data.py',
-           'scripts/make_hk_fits.py',
-           'scripts/modulator_commander.py',
-           'scripts/run_bot.py',
-           'scripts/run_hkserver.py',
-           'scripts/copy2cc.py',
-           'scripts/copy2jupyter.py']
-if len(sys.argv)>1 and sys.argv[1]=='install':
-    print('installing executable scripts...')
-    for F in scripts:
-        basename = os.path.basename(F)
-        cmd = 'rm -f %s/%s; cp -puv %s %s;chmod +x %s/%s' % (exec_dir,basename,F,exec_dir,exec_dir,basename)
-        proc=subprocess.Popen(cmd,stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        out,err=proc.communicate()
-        if out:print(out.strip())
-        if err:print(err.strip())
+msg  = "QUBICPACK is now in a separate repository!\n"
+msg += "Please create a new local respository with:\n"
+msg += "$ git clone https://github.com/satorchi/qubicpack.git\n\n"
+print(msg)
 
